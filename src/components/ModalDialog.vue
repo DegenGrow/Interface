423d95eb2518698110fe52c806d7bf4802c7c3f6
Degen Grow <contact@degengrow.xyz>
2022-10-18 11:20:52 +0100

    Update ModalDialog.vue

--- src/components/ModalDialog.vue
+++ src/components/ModalDialog.vue
@@ -11,11 +11,7 @@
 
   <ButtonWhite text="Get $DGROW" />
 
-  <ButtonWhite
-    text="Staking Dapp"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-  />
+  <ButtonWhiteLink text="Spend" />
 
   <!-- Modal -->
   <div
@@ -38,6 +34,28 @@
             aria-label="Close"
           ></button>
         </div>
+        <div class="modal-body">
+          <p>
+            It seems you are a lost traveler, but you have a compass, it will
+            show the way. Even among the many monophonic threads that enveloped
+            the view, having considered among them only one with slight
+            differences, will lead you to where the sunset is coming.
+          </p>
+        </div>
+        <div class="p-3">
+          <form>
+            <div class="form-floating">
+              <input
+                type="text"
+                class="form-control border-start-0 border-end-0 border-top-0 shadow-none rounded-0 bg-transparent text-white"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput"> Enter secret key</label>
+            </div>
+            <ButtonWhite text="Submit Secret Key" />
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -46,11 +64,13 @@
 <script>
 // import ButtonComponent from "../components/Button.vue";
 import ButtonWhite from "../components/ButtonWhite.vue";
+import ButtonWhiteLink from "./ButtonWhiteLink.vue";
 export default {
   name: "ModalDialog",
   components: {
     // ButtonComponent,
     ButtonWhite,
+    ButtonWhiteLink,
   },
 };
 </script>
