8d3fd4e3342358b0b7949bcf4cb293052747e5f8
Degen Grow <contact@degengrow.xyz>
2022-10-18 11:17:47 +0100

    Update ButtonWhite.vue

--- src/components/ButtonWhite.vue
+++ src/components/ButtonWhite.vue
@@ -21,29 +21,3 @@ export default {
   color: black;
 }
 </style>
-
-<template>
-  <a href="https://app.uniswap.org/#/swap?outputCurrency=0x46ce776bca835d4f704072c28ab1d9a54f4b7b46">
-    <button class="btn px-3 mt-4">{{ text }}</button></a
-  >
-</template>
-
-<script>
-export default {
-  name: "ButtonWhiteLnk",
-  props: {
-    text: String,
-  },
-};
-</script>
-<style scoped>
-.btn {
-  border: 1px solid white;
-  border-radius: 0 !important;
-  color: white;
-}
-.btn:hover {
-  background-color: white;
-  color: black;
-}
-</style>
